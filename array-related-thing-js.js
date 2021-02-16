fe3b5e42a3c38c93df46c16ae59b7ba19de47ae0
Mariam <mariamchowdhury.200@gmail.com>
2021-02-17 00:34:34 +0600

    forEach-ended

--- array-related-thing-js.js
+++ array-related-thing-js.js
@@ -1,5 +1,4 @@
-//map - eita always array te korte hoy. array baad e onno kisu te kora jay na!ekta array r onk gula property thakte pare . if ami jekono ekta niye kaj korte chai tokhon map korte hoy.
-
+//map
 const items=[
   {id:01, name:'phone',price:700},
   {id:02, name:'pc',price:7000},
@@ -11,3 +10,9 @@ const names=items.map(product=>product.name);
 const prices=items.map(p=>p.price);
 console.log(names);
 console.log(prices);
+
+//forEach
+items.forEach(n=>console.log(n.name));
+
+//filter
+
